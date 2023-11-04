a4d390e0c0ff2fbe5d6e059c0e50b6e57b4a703d
Diego Leonardo Gloria Salas <dlgloriasalas@gmail.com>
2023-11-04 13:23:35 -0300

    test unitarios

---
+++ GestionEmpleados.Test/EmpleadosTest.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrimerParcial;
+using System;
+
+namespace GestionEmpleados.Test
+{
+    [TestClass]
+    public class EmpleadosTest
+    {
+        [TestMethod]
+        public void OperadorIgual_DeberiaRetornarTrue_CuandoEmpleadosSonIgualesPorNombreYTurno()
+        {
+            // Arrange
+            Mesero empleadoUno = new Mesero("Juan", 1111, ETurnos.MañanaPartTime, "Patio", 4);
+            Mesero empleadoDos = new Mesero("Juan", 2222, ETurnos.MañanaPartTime, "Principal", 10);
+
+            //Act y Assert
+            Assert.AreEqual(empleadoUno, empleadoDos);
+        }
+
+        [TestMethod]
+        public void OperadorIgual_DeberiaRetornarTrue_CuandoEmpleadosSonIgualesPorLegajo()
+        {
+            // Arrange
+            Mesero empleadoUno = new Mesero("Juan", 1111, ETurnos.MañanaPartTime, "Patio", 4);
+            Mesero empleadoDos = new Mesero("Laura", 1111, ETurnos.NochePartTime, "Patio", 4);
+
+            //Act y Assert
+            Assert.AreEqual(empleadoUno, empleadoDos);
+        }
+
+        [TestMethod]
+        public void OperadorIgual_DeberiaRetornarFalse_CuandoEmpleadosSonDistintosPorNombreYTurnoOLegajo()
+        {
+            // Arrange
+            Mesero empleadoUno = new Mesero("Juan", 1111, ETurnos.MañanaPartTime, "Principal", 10);
+            Mesero empleadoDos = new Mesero("Fernando", 2222, ETurnos.MañanaPartTime, "Principal", 10);
+            Mesero empleadoTres = new Mesero("Leo", 3333, ETurnos.MañanaPartTime, "Segundo piso", 3);
+            Mesero empleadoCuatro = new Mesero("Leo", 4444, ETurnos.TardeFullTime, "Segundo piso", 3);
+
+            //Act y Assert
+            Assert.AreNotEqual(empleadoUno, empleadoDos);
+            Assert.AreNotEqual(empleadoTres, empleadoCuatro);
+        }
+
+        [TestMethod]
+        public void ConversionImplicitaString_DeberiaRetornarNombreCorrecto()
+        {
+            // Arrange
+            Cajero empleado = new Cajero("Franco", 1234, ETurnos.TardePartTime, 0, 1);
+
+            // Act
+            string nombre = empleado;
+
+            // Assert
+            Assert.AreEqual("Franco", nombre);
+        }
+
+        [TestMethod]
+        public void ConversionImplicitaInt_DeberiaRetornarLegajoCorrecto()
+        {
+            // Arrange
+            Cajero empleado = new Cajero("Franco", 1234, ETurnos.TardePartTime, 0, 1);
+
+            // Act
+            int legajo = empleado;
+
+            // Assert
+            Assert.AreEqual(1234, legajo);
+        }
+
+        [TestMethod]
+        public void ConversionImplicitaETurnos_DeberiaRetornarTurnoCorrecto()
+        {
+            // Arrange
+            Cajero empleado = new Cajero("Franco", 1234, ETurnos.TardePartTime, 0, 1);
+
+            // Act
+            ETurnos turno = empleado;
+
+            // Assert
+            Assert.AreEqual(ETurnos.TardePartTime, turno);
+        }
+
+    }
+}
---
+++ GestionEmpleados.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GestionEmpleados.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GestionEmpleados.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ca24de1f-e49b-4bf7-8569-24021c5cfb78")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
